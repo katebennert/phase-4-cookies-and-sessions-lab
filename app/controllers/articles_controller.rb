f9f0e9e94c4fcefa6bcae7a529b26789942842ea
Kate Bennert <kate.bennert@gmail.com>
2023-05-04 14:42:05 -0400

    commit tree b03949e86a1a9296767a9d0fc7c9826244ab730e

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,20 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+    session[:page_views] ||= 0
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
     article = Article.find(params[:id])
-    render json: article
+    if session[:page_views] <= 3
+      render json: article
+    else 
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
